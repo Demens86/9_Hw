7a3066750166b469b63d49abe6bfe709134b21a0
Demens86 <denkurpita86rus@yandex.ru>
2026-06-14 22:13:14 +0500

    prac some method read json

--- internal/LoadFile.go
+++ internal/LoadFile.go
@@ -1,7 +1,10 @@
 package internal
 
 import (
+	"bytes"
 	"encoding/json"
+	"io"
+	"log"
 	"os"
 	"prac/modules"
 )
@@ -12,9 +15,18 @@ func LoadFile(path string) error {
 		return err
 	}
 
-	err1 := json.Unmarshal(data, &modules.Employees)
-	if err1 != nil {
-		return err1
+	//err1 := json.Unmarshal(data, &modules.Employees)
+	//if err1 != nil {
+	//	return err1
+	//}
+	dec := json.NewDecoder(bytes.NewReader(data))
+	for {
+		if err := dec.Decode(&modules.Employees); err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+
 	}
 
 	return nil
